0f1a9b59b4c89fd6256c9a8b32c0bbc5884b9e6a
Lukáš Grejták <lukigrejtak@gmail.com>
2023-10-20 16:42:29 +0200

    1st commit

---
+++ main.py
@@ -0,0 +1,174 @@
+import requests
+import csv
+import re
+import os
+from time import sleep
+from bs4 import BeautifulSoup
+
+URL = "https://liquipedia.net/counterstrike/Portal:Players"
+# Fake HTML header with User-Agent to mimic a real browser request
+HEADER = {
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) "
+                  "AppleWebKit/537.36 (KHTML, like Gecko) "
+                  "Chrome/98.0.4758.102 Safari/537.36"
+}
+
+
+# Get base URL of the site from given URL
+########################################################################################################################
+def get_base_url(input_url):
+    parts = input_url.split('/')
+    output = parts[0] + "//" + parts[2]
+    return output
+
+
+# Check if given URL is allowed via robots.txt of the site
+########################################################################################################################
+base_url = get_base_url(URL)
+
+sleep(30)
+robots = requests.get(base_url + "/robots.txt", headers=HEADER).text
+robots = robots.split("User-agent: *\n")[1].split("User-agent:")[0].split("Disallow: ")[1:]
+
+for i, item in enumerate(robots):
+    robots[i] = item.strip()
+
+if URL[len(base_url):] not in robots:
+    pass
+else:
+    print("I am not allowed to visit this URL.")
+    exit(0)
+
+
+# Fetch HTML code
+########################################################################################################################
+def fetch_urls(url, header):
+    sleep(30)
+    response = requests.get(url, headers=header)
+    soup = BeautifulSoup(response.content, 'html.parser').prettify()
+
+    # Regex to find all href attributes inside of HTML
+    pattern = r'href="([^"]+)"'
+    hrefs = re.findall(pattern, soup)
+
+    # Regex to find only these hrefs, that start with /counterstrike/Portal:Players/ to fetch all regions tabs URLs
+    pattern = r'/counterstrike/Portal:Players/.*'
+    for href in hrefs:
+        if re.match(pattern, href):
+            save_to_txt(get_base_url(url) + href, "", "region_url")
+
+    # V pripade maleho počtu dat, moznost este ziskat dalsich hracov z /counterstrike/Banned_players,
+    # NOTE: player bez profilu nemá v Title "(page does not exist)"
+    # Regex to find only the href, that starts with /counterstrike/Banned_players to fetch the "Banned" tab URL
+    # pattern = r'/counterstrike/Banned_players.*'
+    # for href in hrefs:
+    #     if re.match(pattern, href):
+    #         save_to_txt(get_base_url(url) + href, "", "ban_url")
+    #         sleep(30)
+    #         response = requests.get(get_base_url(url) + href, headers=header)
+    #         soup = BeautifulSoup(response.content, 'html.parser').prettify()
+    #
+    #         pattern = r'<div\s+class="divRow mainpage-transfer-neutral"[^>]*>[\s\S]*?<\/div>'
+    #
+    #         players = re.findall(pattern, soup)
+    #
+    #         for player in players:
+    #             print(player)
+    # exit()
+
+    print("Extracting of starting URLs completed")
+
+    regions_urls = open('regions.txt', 'r', encoding='utf-8')
+    for region in regions_urls:
+        sleep(30)
+        response = requests.get(region, headers=header)
+        soup = BeautifulSoup(response.content, 'html.parser').prettify()
+        print(f"[INFO] Visiting {region}...")
+
+        pattern = r'<div class="template-box"(.*?)</div>'
+        countries = re.findall(pattern, soup, re.DOTALL)
+
+        for country in countries:
+            country_pattern = r'<th[^>]*>\s*<span[^>]+>\s*<img[^>]+>\s*</span>\s*([^<]+)\s*</th>'
+            country_name = re.search(country_pattern, country).group(1).strip()
+
+            pattern = r'<a\s+href="([^"]+)"[^>]*>[^<]+</a>\s*-\s*'
+            players = re.findall(pattern, country)
+
+            player_names = [re.search(r'/counterstrike/([^/]+)', player).group(1) for player in players]
+
+            print("[INFO] Browsing players from " + country_name + ":")
+            print(', '.join(player_names))
+
+            for player in players:
+                save_to_txt(get_base_url(url) + player, "", "player_url")
+
+
+def save_htmls(header):
+    players_urls = open('players.txt', 'r', encoding='utf-8').readlines()
+
+    for index, player_url in enumerate(players_urls):
+        url = player_url.rstrip()
+
+        sleep(30)
+        response = requests.get(url.rstrip(), headers=header)
+        soup = BeautifulSoup(response.content, 'html.parser').prettify()
+
+        save_to_txt(url, soup, "html")
+        print(f"[{index + 1}/{len(players_urls)}]")
+        print(f'Successfully saved HTML content from {url}')
+
+
+def parse_html(file):
+    pass
+    # TODO
+
+
+# Saving different content into text file
+########################################################################################################################
+def save_to_txt(url, content, task):
+    # Check if files exist, create them if not
+    for file in ['players.txt', 'regions.txt', 'bans.txt', 'scraped_urls.txt', 'scraped_htmls.txt']:
+        if not os.path.exists(file):
+            with open(file, 'w', encoding='utf-8') as f:
+                pass  # This creates an empty file
+
+    if task == "player_url":
+        # Save URL to a TXT file
+        with open('players.txt', 'r', encoding='utf-8') as f_read:
+            if url not in f_read.read():
+                with open('players.txt', 'a', encoding='utf-8') as f_append:
+                    f_append.write(url + '\n')
+
+    elif task == "region_url":
+        # Save URL to a TXT file
+        with open('regions.txt', 'r', encoding='utf-8') as f_read:
+            if url not in f_read.read():
+                with open('regions.txt', 'a', encoding='utf-8') as f_append:
+                    f_append.write(url + '\n')
+
+    elif task == "ban_url":
+        # Save URL to a TXT file
+        with open('bans.txt', 'r', encoding='utf-8') as f_read:
+            if url not in f_read.read():
+                with open('bans.txt', 'a', encoding='utf-8') as f_append:
+                    f_append.write(url + '\n')
+
+    elif task == "html":
+        # Save HTML content to a TXT file
+        with open('scraped_urls.txt', 'r+', encoding='utf-8') as f_read:
+            if url not in f_read.read():
+                f_read.write(url + '\n')
+                with open('scraped_htmls.txt', 'a', encoding='utf-8') as f_append:
+                    f_append.write(f"URL: {url}\n")
+                    f_append.write("HTML Content:\n")
+                    f_append.write(content)
+
+
+def main():
+    global URL, HEADER
+    fetch_urls(URL, HEADER)
+    save_htmls(HEADER)
+
+
+main()
