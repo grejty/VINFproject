2ed48c1f6e30f6e70335f94caaf7109c1cc6dd9f
Lukáš Grejták <lukigrejtak@gmail.com>
2023-11-19 01:29:43 +0100

    sparkParser kinda works + docker file update

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
 FROM coady/pylucene
 WORKDIR /usr/src/app
+RUN pip install pyspark
+RUN pip install pandas
 COPY . .
 CMD ["python3", "-m", "http.server", "80"]
 
---
+++ spark/sparkParser.py
@@ -0,0 +1,46 @@
+from pyspark.sql import SparkSession
+import pyspark.sql.functions as f
+import pandas as pd
+
+# Create a SparkSession with the Spark XML package
+spark = SparkSession.builder \
+    .appName("Wikipedia XML Parsing") \
+    .config("spark.jars.packages", "com.databricks:spark-xml_2.12:0.14.0") \
+    .getOrCreate()
+
+# Read the parsed_data CSV file into a DataFrame
+df_parsed_data = spark.read.csv("../data/parsed_data.csv", header=True, sep="\t")
+
+# Extract the 'Nick' column values into a list
+nicks_list = df_parsed_data.select("Nick").rdd.flatMap(lambda x: x).collect()
+
+dump_df = spark.read.format('xml') \
+    .option("rowTag", "page") \
+    .load("../wikidumps/articles11.xml-p5399367p6899366")
+
+dump_df.printSchema()
+
+# Show titles
+# dump_df.select("titles").show(n=20, truncate=False)
+
+#####################
+
+# Save the DataFrame locally
+# dump_df.repartition(1).write.options(header="True", delimiter=",").mode("overwrite").csv("output")
+
+filtered_df = (
+    dump_df
+    .filter(f.col("title").isin(nicks_list))
+    #.filter(f.col("revision.text._VALUE").contains("team_history"))
+    .withColumn("team_history", f.regexp_extract("revision.text._VALUE", r'team_history\s*=\s*(.*?)(\n|$)', 1))
+)
+
+
+# Select and show the desired columns
+result_df = filtered_df.select("title", "team_history")
+result_df.show(truncate=False)
+
+#filtered_df.repartition(1).write.options(header="True", delimiter=",").mode("overwrite").csv("output") TODO
+
+# Stop the SparkSession
+spark.stop()
